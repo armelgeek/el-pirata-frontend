898376462ad88f1e816af095512b22fd4c0f974e
Hajavololona Armellin <armelgeek5@gmail.com>
2025-03-12 07:56:48 +0300

    feat: ajout de husky et commit lint

---
+++ .commitlintrc.js
@@ -0,0 +1,16 @@
+export default {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore']
+        ],
+        'type-case': [2, 'always', 'lowercase'],
+        'type-empty': [2, 'never'],
+        'subject-case': [2, 'always', ['sentence-case', 'lowercase']],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'header-max-length': [2, 'always', 72]
+    }
+};
